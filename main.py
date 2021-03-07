aa070fd0130747074e45c11bcd6d83336a9bc582
Caleb Ng <caleb17ng@gmail.com>
2021-03-06 22:03:01 -0800

    1

---
+++ main.py
@@ -0,0 +1,89 @@
+import cv2
+import os
+
+def detect_label(path):
+    import io
+
+    # Imports the Google Cloud client library
+    from google.cloud import vision
+
+    # Instantiates a client
+    client = vision.ImageAnnotatorClient()
+
+    # The name of the image file to annotate
+    file_name = path
+
+    # Loads the image into memory
+    with io.open(file_name, 'rb') as image_file:
+        content = image_file.read()
+
+    image = vision.Image(content=content)
+
+    # Performs label detection on the image file
+    response = client.label_detection(image=image)
+    labels = response.label_annotations
+
+    print('Labels:')
+    for label in labels:
+        print(label.description)
+
+
+def detect_faces(path):
+    """Detects faces in an image."""
+    from google.cloud import vision
+    import io
+    client = vision.ImageAnnotatorClient()
+
+    with io.open(path, 'rb') as image_file:
+        content = image_file.read()
+
+    image = vision.Image(content=content)
+
+    response = client.face_detection(image=image)
+    faces = response.face_annotations
+
+    # Names of likelihood from google.cloud.vision.enums
+    likelihood_name = ('UNKNOWN', 'VERY_UNLIKELY', 'UNLIKELY', 'POSSIBLE',
+                       'LIKELY', 'VERY_LIKELY')
+    print('Faces:')
+
+    for face in faces:
+        print('anger: {}'.format(likelihood_name[face.anger_likelihood]))
+        print('joy: {}'.format(likelihood_name[face.joy_likelihood]))
+        print('surprise: {}'.format(likelihood_name[face.surprise_likelihood]))
+
+        vertices = (['({},{})'.format(vertex.x, vertex.y)
+                    for vertex in face.bounding_poly.vertices])
+
+        print('face bounds: {}'.format(','.join(vertices)))
+
+    if response.error.message:
+        raise Exception(
+            '{}\nFor more info on error messages, check: '
+            'https://cloud.google.com/apis/design/errors'.format(
+                response.error.message))
+
+
+cap = cv2.VideoCapture(0)
+directory = r'C:\Users\caleb\Pictures\test'
+os.chdir(directory)
+
+while(1):
+    ret, frame = cap.read()
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(1) & 0xFF == ord('l'):
+        cv2.imwrite('file.jpg', frame)
+        detect_label(r'C:\Users\caleb\Pictures\test\file.jpg')
+
+    ##if cv2.waitKey(1) & 0xFF == ord('f'):
+        ##cv2.imwrite('file.jpg', frame)
+        ##detect_faces(r'C:\Users\caleb\Pictures\test\file.jpg')
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
